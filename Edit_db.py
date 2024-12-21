17ac0d78dc159ff442c85f17d4f0adf22d561042
shmlv9 <denis2009shumiov@gmail.com>
2024-12-22 00:17:43 +0300

    Add database editor

---
+++ Edit_db.py
@@ -0,0 +1,52 @@
+import sqlite3
+from PyQt6 import uic
+from PyQt6.QtWidgets import QMainWindow
+
+
+class Edit_DB(QMainWindow):
+    def __init__(self, func):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.func = func
+        self.label_8.setVisible(False)
+        if func == 'edit':
+            self.label_8.setVisible(True)
+            self.pushButton.setText('Редактировать')
+        else:
+            self.pushButton.setText('Добавить')
+        self.pushButton.clicked.connect(self.change)
+
+    def change(self):
+        self.statusBar().showMessage('')
+        f = sqlite3.connect('coffee.sqlite')
+        cur = f.cursor()
+        if self.func == 'update':
+            try:
+                if all([self.id.text(), self.name.text(), self.roasting.text(), self.grounds.text(), self.taste.text(),
+                        self.price.text(), self.size.text()]):
+                    cur.execute(f'INSERT INTO Coffee(id, name, roasting, "Ground/in_grains", taste, price, size) '
+                                f'VALUES({self.id.text()}, "{self.name.text()}", "{self.roasting.text()}", '
+                                f'"{self.grounds.text()}", "{self.taste.text()}", "{self.price.text()}", '
+                                f'"{self.size.text()}")')
+                    f.commit()
+                    f.close()
+                    self.destroy()
+                else:
+                    self.statusBar().showMessage('Вы указали не все значения')
+            except sqlite3.IntegrityError:
+                self.statusBar().showMessage('Запись с таким id уже есть')
+        else:
+            if all([self.id.text(), self.name.text(), self.roasting.text(), self.grounds.text(), self.taste.text(),
+                    self.price.text(), self.size.text()]):
+                cur.execute(f'UPDATE Coffee SET name = "{self.name.text()}", '
+                            f'roasting = "{self.roasting.text()}", '
+                            f'"Ground/in_grains" = "{self.grounds.text()}", '
+                            f'taste = "{self.taste.text()}", '
+                            f'price = "{self.price.text()}", '
+                            f'size = "{self.size.text()}"'
+                            f'WHERE id = {self.id.text()}')
+                f.commit()
+                f.close()
+                self.destroy()
+            else:
+                self.statusBar().showMessage('Вы указали не все значения')
--- main.py
+++ main.py
@@ -2,12 +2,28 @@ import sqlite3
 import sys
 from PyQt6 import uic
 from PyQt6.QtWidgets import QMainWindow, QTableWidgetItem, QApplication
+from Edit_db import Edit_DB
 
 
 class Main(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
+        self.update.clicked.connect(self.update_f)
+        self.edit.clicked.connect(self.edit_f)
+        self.update_table.clicked.connect(self.load_table)
+        self.load_table()
+
+    def update_f(self):
+        self.form = Edit_DB('update')
+        self.form.show()
+
+    def edit_f(self):
+        self.form = Edit_DB('edit')
+        self.form.show()
+
+    def load_table(self):
+        self.tableWidget.setRowCount(0)
         f = sqlite3.connect('coffee.sqlite')
         cur = f.cursor()
         result = cur.execute('SELECT * FROM Coffee').fetchall()
